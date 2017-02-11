7b7f8cddb20463ee8cc9e291e0f6a86387002723
Maryn Conen <marynconen@outlook.com>
2017-02-11 16:16:10 +0100

    first commit, setting up tdd framework

--- Expost.GitHubConnectivity/Properties/AssemblyInfo.cs
+++ Expost.GitHubConnectivity/Properties/AssemblyInfo.cs
@@ -1,15 +1,17 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Expost.Requirements")]
+[assembly: AssemblyTitle("Expost.GitHubConnectivity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Expost.Requirements")]
+[assembly: AssemblyProduct("Expost.GitHubConnectivity")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("a38fd0d8-a2bf-4b53-8a8d-759a8141c88c")]
+[assembly: Guid("986cae27-1a78-405e-9150-36e70156c475")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Expost.GitHubConnectivity")]
---
+++ Expost.Requirements/PullingCommitInformationFromGitHub.cs
@@ -0,0 +1,17 @@
+﻿namespace Expost.Requirements {
+
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PullingCommitInformationFromGitHub {
+
+        [Test]
+        public void ShowCommitsFromThisProject() {
+            IEnumerable<string> commits = new List<string>();
+            Assert.That(() => commits.Count(), Is.GreaterThan(0));
+        }
+
+    }
+}
