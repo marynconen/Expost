d54700007fcaba07aa2490b21636dc90386eb823
Maryn Conen <marynconen@outlook.com>
2017-02-11 15:47:50 +0100

    Add project files.

---
+++ Expost.Requirements/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Expost.Requirements {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
